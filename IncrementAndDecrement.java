3fa36d6f9956dff09455fe21ca2b9b575f9511d6
satya121994 <ksatyanarayana615@gmail.com>
2022-04-04 18:39:50 +0530

    test

---
+++ IncrementAndDecrement.java
@@ -0,0 +1,47 @@
+package JavaBasics;
+
+public class IncrementAndDecrement {
+
+	public static void main(String[] args) {
+		     
+		 //post Increment
+			int a = 10;
+			int b = a++;
+			
+			System.out.println(a);//11
+			System.out.println(b);//10
+		
+			
+			//pre increment
+		 	
+		   int i = 6;
+		   int j = ++i;
+		   
+		   System.out.println(i);//7
+		   System.out.println(j);//7
+		   
+		   //post Decrement
+		   
+		   int m = 9;
+		   int n = m--;
+		   
+		   System.out.println(m);//8
+		   System.out.println(n);//9
+		
+		   //pre Decrement
+		   
+		int p = 33;
+		int q = --p;
+		
+		
+		System.out.println(p);//32
+		System.out.println(q);//32
+		
+		
+		
+		
+		
+		
+			}
+
+}
